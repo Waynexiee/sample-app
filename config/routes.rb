a2b044435b30449523bd171459d82ca1714aec6d
Waynee <xiewayne1993@gmail.com>
2017-05-21 17:55:09 +0800

    Implement advanced login

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   root 'static_pages#home'
+
   get 'say/hello'
 
   get 'say/goodbye'
@@ -20,6 +21,8 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
 
   delete '/logout', to: 'sessions#destroy'
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
